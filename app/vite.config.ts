49ee220731b70d00f4f288ae331f06be97362271
Amir Kabiri <akabiridev@gmail.com>
2025-01-23 13:59:32 +0330

    stuff

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+function customImageLoader() {
+  return {
+    name: 'custom-image-loader',
+    async transform(src, id) {},
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
